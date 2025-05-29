fb46f6983a0aab1024e000ef24a8ccfa38ef7676
trh4ckn0n <trhacknon@proton.me>
2025-05-30 01:35:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ results = {}
 def generate_onion(prefix):
     try:
         result = subprocess.run(["oniongen", f"^{prefix}", "5"], capture_output=True)
-        output = result.stdout.decode().strip()
+        output = result.stdout.decode(errors='ignore').strip()
 
         if output:
             onion_address = output.split("\n")[0]
